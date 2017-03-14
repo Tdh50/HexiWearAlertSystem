2d5b83e7a3f9a18abb9732e90031180688029dd1
DeanBritt <DeanBritt@localhost>
2017-03-14 15:48:36 +0000

    New alert screen;

--- images.h
+++ images.h
@@ -5,4 +5,4 @@ extern const uint8_t  FallDet_bmp[18438];
 extern const uint8_t  FallDetPage_bmp[18438];
 extern const uint8_t  Home_bmp[18438];
 extern const uint8_t  HeartRatePage_bmp[18438];
-extern const uint8_t  Alert_bmp[7206];
+extern const uint8_t  Alert_bmp[18438];
